3e72c585f6aa1b0795950d17168c58224ddc8d42
Muhammad Usama <dev.usama.visnext@gmail.com>
2021-07-05 19:13:23 +0500

    Assosiation 1 to Many

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 class RecipeTest < ActiveSupport::TestCase
 
 	def setup
-		@recipe = Recipe.new(name: "vegetable", description: "great vegetable")
+		@chef = Chef.create!(name: "usama", email: "usama@gmail.com")
+		@recipe = @chef.recipes.build(name: "vegetable", description: "great vegetable")
+	end
+
+	test "recipe without chef is invalid" do
+		@recipe.chef_id = nil
+		assert_not @recipe.valid?
 	end
 
 	test "recipe should be valid" do
