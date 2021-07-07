76c59f137e4e47e760211560db562d481f385f34
Muhammad Usama <dev.usama.visnext@gmail.com>
2021-07-07 11:33:31 +0500

    Sign up Chef

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_133942) do
+ActiveRecord::Schema.define(version: 2021_07_06_152855) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "recipes", force: :cascade do |t|
